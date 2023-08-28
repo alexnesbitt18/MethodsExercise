cbd302e07104b3e894b383182c33ea525d2733ce
alexnesbitt18 <alexnesbitt18@gmail.com>
2023-08-28 09:57:01 -0700

    Added parameters for math problems

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace MethodsExercise
+﻿using System.Collections.Generic;
+
+namespace MethodsExercise
 {
     public class Program
     {
+        private static int sum;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello there, what is your name?");
@@ -20,7 +24,44 @@
             Console.WriteLine($"After a moment of hesitation, {name} joined in on the chorus and spent the rest of the day with the {animal}.");
             Console.WriteLine($"The End.");
 
+            Console.WriteLine($"What number would you like to start with?");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"What second number would you like to add the first number with?");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum for these two numbers would be {sum}");
+
+            Console.WriteLine($"What number would you like to start with?");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine($"What second number would you like to add the first number with?");
+            int y = int.Parse(Console.ReadLine());
+
+            int product = Multiply(num1, num2);
+            Console.WriteLine($"The total for these two numbers would be {product}");
 
         }
+        public static int Sum(params int[] list)
+        { 
+            for(int i = 0; i < list.Length; i++)
+            {
+                sum = sum + list[i];
+            }
+            return sum;
+        }
+     
+        public static int Subtraction(int num1, int num2)
+        { 
+            return num1 - num2;
+        }
+        public static int Multiply (int x, int y)
+        {
+            return x * y;
+        }
+        public static int Division (int x, int y)
+        {
+            return x / y;
+        }
     }
+
 }
